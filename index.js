d1f75565c3d87797576d34065faafdabd6f089ee
DedsecRetr0-loop <3156452620@qq.com>
2021-09-17 17:25:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("我们在寻找宇宙人，未来人和超能力者");
+alert("你是宇宙人，未来人或者超能力者吗？")
+let confirm = confirm("请确定！！！")
+console.log(confirm)
+let input = prompt("来文学社")
+console.log(prompt)
+let age = 17//number
+let 神 = "凉宫春日"//string
+let is宇宙人 = true
+console.log("你好" + 神 + "你今年" + age + "了")
